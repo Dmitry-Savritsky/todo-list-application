a9bcef4c31b0cbe1da059bbef41003355c9783e6
Dmitry <dumon.sav@gmail.com>
2018-04-13 08:19:41 +0300

    Added entry point

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import { render } from 'react-dom';
+
+render(
+    <div>Initial version </div>,
+    document.getElementById('root'),
+);
