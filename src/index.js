d514be45ebb63b2ed96c5585a5c4a486a1843e15
Dmitry <dumon.sav@gmail.com>
2018-06-18 20:53:07 +0300

    Added router to the entry point

--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import ConnectedApp from '../containers/ConnectedApp.jsx';
+import { createBrowserHistory } from 'history';
+import { syncHistoryWithStore } from 'react-router-redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import rootReducer from '../reducers'
+import TaskEditPage from '../pages/TaskEditPage.jsx';
+import MainPage from '../pages/MainPage.jsx';
 
 const store = createStore(rootReducer);
+const history = syncHistoryWithStore(createBrowserHistory(), store);
+window.store = store;
 
 render(
     <Provider store={store}>
-        <ConnectedApp />
+        <BrowserRouter history={history}>
+            <Switch>
+                <Route exact path='/' component={MainPage} />
+                <Route path="/task/:id" component={TaskEditPage} />
+            </Switch>
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root'),
 );
