8c7a74589ae4bc7ba82d49207f0fdfa51538bf0c
Dmitry <dumon.sav@gmail.com>
2018-06-21 12:13:02 +0300

    Added deletion of tasks which was in deleted categories

--- reducers/main.js
+++ reducers/main.js
@@ -27,19 +27,6 @@ const initialState = {
         {
             title: "Category 2",
             id: "catID2",
-            tasks: [{
-                name: "task 3",
-                id: "id3",
-                checked: false,
-                description: "description 3"
-            },
-            {
-                name: "task 4",
-                id: "id4",
-                checked: true,
-                description: "description 4"
-            },
-            ],
             nestedCategories: [],
         },
     ],
@@ -65,7 +52,20 @@ const initialState = {
         checked: false,
         description: "description 2"
     },
-
+    {
+        parentId: "catID2",
+        id: "id5",
+        name: "task 3",
+        checked: false,
+        description: "description 3"
+    },
+    {
+        parentId: "catID2",
+        id: "id4",
+        name: "task 5",
+        checked: true,
+        description: "description 4"
+    },
     ],
 };
 
@@ -80,11 +80,18 @@ export default function main(state = initialState, action) {
             }
 
         case ACTIONS.DELETE_CATEGORY: {
+            let category = findCategoryById(state.categories, action.id);
+            let idArray = findNestedCategoriesId(category);
+            let tasks = deleteTasks(state.tasks, idArray);
+
             return {
                 ...state,
                 categories: [
-                    ...applyDeleteCategory(state.categories, action.id)
+                    ...applyDeleteCategory(state.categories, action.id),
                 ],
+                tasks: [
+                    ...tasks,
+                ]
             };
         }
 
@@ -247,7 +254,9 @@ function applyDeleteCategory(category, id) {
     let result;
 
     filtered = category.filter(element => {
-        if (element.id == id) completed = true;
+        if (element.id == id) {
+            completed = true;
+        }
         return element.id != id;
     });
 
@@ -261,4 +270,38 @@ function applyDeleteCategory(category, id) {
     }
 
     return result;
+}
+
+function findCategoryById(category, id) {
+    let result = null;
+    category.forEach(element => {
+        if (element.id == id) result = element;
+    });
+
+    if (result == null) {
+        category.forEach(element => {
+            let stepResult = findCategoryById(element.nestedCategories, id);
+            if (stepResult != null) result = stepResult;
+        })
+    }
+
+    return result;
+}
+
+function findNestedCategoriesId(category) {
+    let result = [];
+    result.push(category.id);
+    category.nestedCategories.forEach(element => {
+        result = result.concat(findNestedCategoriesId(element));
+    });
+    return result;
+}
+
+function deleteTasks(tasks, idArray) {
+    return tasks.filter(element => {
+        for (let i = 0; i < idArray.length; i++) {
+            if (element.parentId == idArray[i]) return false;
+        }
+        return true;
+    });
 }
